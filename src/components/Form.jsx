7c1db03fbf131e1d5f4410aa2208babeb80b90eb
tanmoy-og <official.tanmoychoudhury@protonmail.ccom>
2026-01-23 13:31:30 +0530

    favicon added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Form = () => {
   return (
     <div className="bg-60W text-10B basis-1/2 px-32 pt-5 flex flex-col justify-center">
-        <form action="" className='flex flex-col gap-8 mx-auto w-full max-w-[600px]'>
+        <div action={null} className='flex flex-col gap-8 mx-auto w-full max-w-[600px]'>
             <div className="flex flex-col gap-3">
                 <h1 className='text-4xl tracking-tight'>Contact sales form</h1>
                 <h1 className='text-30B'>Please fill out the form below to get started</h1>
@@ -19,14 +19,14 @@ const Form = () => {
                 </div>
             </div>
             <div className="flex justify-between items-center py-6">
-                <button className='bg-30B text-60W rounded-full hover:cursor-not-allowed active:scale-98 px-4 py-2.5'>Next</button>
+                <button className='bg-30B text-60W rounded-full hover:cursor-not-allowed active:scale-98 px-4 py-2.5' onClick={null}>Next</button>
                 <div className="flex gap-2">
                     <div className="bg-10B h-1 w-18 rounded-full"></div>
                     <div className="bg-10F h-1 w-18 rounded-full"></div>
                     <div className="bg-10F h-1 w-18 rounded-full"></div>
                 </div>
             </div>
-        </form>
+        </div>
     </div>
   )
 }
